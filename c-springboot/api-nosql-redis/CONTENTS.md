b234683c6c2a975d8e610557cf45312da836a6f3
oh29oh29 <oh29oh29.develop@gmail.com>
2024-10-24 13:53:09 +0900

    zSetOperations(sortedSet)

--- c-springboot/api-nosql-redis/CONTENTS.md
+++ c-springboot/api-nosql-redis/CONTENTS.md
@@ -1,3 +1,15 @@
-> By default, the Spring Boot starter (spring-boot-starter-data-redis) uses Lettuce
+# sorted Set
+ 
+> score 값에 따라 정렬되는 고유한 문자열의 집합
+
+> 순서를 갖는 자료구조이므로 index를 이용해 아이템에 접근할 수 있다.
 > 
-> https://docs.spring.io/spring-boot/how-to/nosql.html
+> index를 이용해 아이템에 접근할 일이 많다면 list가 아닌 sorted set를 사용하는 것도 방법이다.
+> 
+> index를 이용해 데이터에 접근할 때 list는 O(n), sorted set은 O(log(n))
+
+> index로 데이터 조회: ZRANGE command는 기본적으로 index 기반으로 데이터를 조회한다.
+>
+> score로 데이터 조회: ZRANGE command에 BYSCORE 옵션을 사용하면 score를 이용해 데이터를 조회한다.
+>
+> 사전순으로 데이터 조회: ZRANGE command에 BYLEX 옵션을 사욯아면 사전순으로 데이터를 조회한다.  
---
+++ c-springboot/api-nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/apiredis/service/RedisService.kt
@@ -0,0 +1,48 @@
+package com.oh29oh29.devbox.springboot.apiredis.service
+
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.core.ZSetOperations
+import org.springframework.stereotype.Service
+
+@Service
+class RedisService(
+    private val redisTemplate: StringRedisTemplate
+) {
+    private final val zSetOperations: ZSetOperations<String, String> = redisTemplate.opsForZSet()
+
+    init {
+        initialize()
+        setValue()
+        getValue()
+    }
+
+    private fun initialize() {
+        redisTemplate.delete(listOf("main:20240101"))
+    }
+
+    private fun setValue() {
+        zSetOperations.add("main:20240101", "sub:a", 100.0)
+        zSetOperations.add("main:20240101", "sub:b", 50.0)
+        zSetOperations.add("main:20240101", "sub:c", 200.0)
+        zSetOperations.add("main:20240101", "sub:d", 150.0)
+
+        zSetOperations.incrementScore("main:20240101", "sub:d", 1.0)
+    }
+
+    private fun getValue() {
+        println(zSetOperations.range("main:20240101", 0L, -1L))
+        println(zSetOperations.rangeWithScores("main:20240101", 0L, -1L))
+        println(zSetOperations.reverseRange("main:20240101", 0L, -1L))
+        println(zSetOperations.reverseRangeWithScores("main:20240101", 0L, -1L))
+        println()
+        println(zSetOperations.score("main:20240101", "sub:a")!!)
+        println(zSetOperations.score("main:20240101", "sub:b")!!)
+        println(zSetOperations.score("main:20240101", "sub:c")!!)
+        println(zSetOperations.score("main:20240101", "sub:d")!!)
+        println()
+        println(zSetOperations.rank("main:20240101", "sub:a")!!)
+        println(zSetOperations.rank("main:20240101", "sub:b")!!)
+        println(zSetOperations.rank("main:20240101", "sub:c")!!)
+        println(zSetOperations.rank("main:20240101", "sub:d")!!)
+    }
+}
